5791ba14fb081c426da5658e5dce4e13c04210b3
Julius Preuschoff <julius.preuschoff@haw-hamburg.de>
2021-10-27 14:15:53 +0200

    Positionextraktion

--- scanBed.py
+++ scanBed.py
@@ -7,12 +7,49 @@ def reading():
             incomming = s.readline() # reading bytes until \n
             if incomming == bytes('', 'utf-8'): # stop reading if no more messages are comming in
                 break
-            print(str(incomming))    
+            print(str(incomming, 'utf-8'))
+
+
+
+
+def getPosition(serialConnection):
+    # position message example: b'X:19.00 Y:0.00 Z:20.00 E:0.00 Count X:1520 Y:0 Z:8000\n'
+    cmd = bytes('M114' + '\n', 'utf-8') # command for getting the current absolute position
+    serialConnection.write(cmd)
+    incomming = ''
+    incomming = serialConnection.readline() # reading bytes until \n
+    incomming_str = str(incomming, 'utf-8')
+    print("getPos function received: " + incomming_str)
+    
+    # X Position
+    xCharacterPos = incomming_str.find('X')
+    xExtracted = incomming_str[xCharacterPos+2:xCharacterPos+7]
+    xExtracted.rstrip()
+    x = float(xExtracted)
+    
+    # Y position
+    yCharacterPos = incomming_str.find('Y')
+    yExtracted = incomming_str[yCharacterPos+2:yCharacterPos+7]
+    yExtracted.rstrip()
+    y = float(yExtracted)
+    
+    # Z position
+    zCharacterPos = incomming_str.find('Z')
+    zExtracted = incomming_str[zCharacterPos+2:zCharacterPos+7]
+    zExtracted.rstrip()
+    z = float(zExtracted)
+
+    position = [x, y, z]
+    print(position)
+
+
 ################################################################################
 
-from serial.serialutil import Timeout
+# Windows version
+#s = serial.Serial('COM7', 115200, timeout=1) # opening the serial connection to the printer
 
-s = serial.Serial('COM7', 115200, timeout=1) # opening the serial connection to the printer
+# Ubuntu version
+s = serial.Serial('/dev/ttyUSB0', 115200, timeout=1) # opening the serial connection to the printer
 print(s.name)
 
 time.sleep(4) # give him some time
@@ -33,20 +70,21 @@ reading()
 time.sleep(0.5)
 
 # move z axis:
-cmd = bytes('G0 Z20 F500' + '\n', 'utf-8')
+cmd = bytes('G0 Z10 F500' + '\n', 'utf-8')
 s.write(cmd)
 reading()
 time.sleep(0.5)
-for i in range(220):
+for i in range(0, 20, 1):
     print(i)
     cmd = bytes('G0 Y' + str(i) + '\n', 'utf-8')
     s.write(cmd)
     reading()
-    for i in range(220, [4]):
+    for i in range(0, 20, 1):
         print(i)
         cmd = bytes('G0 X' + str(i) + '\n', 'utf-8')
         s.write(cmd)
         reading()
-
-
+    getPosition(s)
+    
+    
 s.close
